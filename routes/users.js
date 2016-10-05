cf4a7e74e857a34290c660cbb9e607a53b7ac9b6
madeleinehuish <vasistha@hotmail.com>
2016-10-05 16:04:32 -0700

    final

--- routes/users.js
+++ routes/users.js
@@ -13,45 +13,46 @@ router.post('/users', (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
 
   if (!firstName || !firstName.trim()) {
-    return next(boom.create(400, 'Email must not be blank'));
+    return next(boom.create(400, 'First name must not be blank'));
   }
 
   if (!lastName || !lastName.trim()) {
-    return next(boom.create(400, 'Email must not be blank'));
+    return next(boom.create(400, 'Last name must not be blank'));
   }
 
   if (!email || !email.trim()) {
     return next(boom.create(400, 'Email must not be blank'));
   }
 
-  knex('users')
-  .where('email', email)
-  .then((result) => {
-    if (result.length > 0) {
-      return next(boom.create(400, 'Email already exists'));
-    }
-  });
-
   if (!password || password.length < 8) {
     return next(boom.create(400, 'Password must be at least 8 characters long'));
   }
 
-  bcrypt.hash(password, 12)
-    .then((hashedPassword) => {
-      const insertUser = { firstName, lastName, email, hashedPassword };
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((result) => {
+      if(result) {
+        return next(boom.create(400, 'Email already exists'));
+      }
 
-      return knex('users').insert(decamelizeKeys(insertUser), '*');
-    })
-    .then((rows) => {
-      const user = camelizeKeys(rows[0]);
+      bcrypt.hash(password, 12)
+        .then((hashedPassword) => {
+          const insertUser = { firstName, lastName, email, hashedPassword };
 
-      delete user.hashedPassword;
+          return knex('users').insert(decamelizeKeys(insertUser), '*');
+        })
+        .then((rows) => {
+          const user = camelizeKeys(rows[0]);
 
-      res.send(user);
-    })
-    .catch((err) => {
-      next(err);
-    });
+          delete user.hashedPassword;
+
+          res.send(user);
+        })
+        .catch((err) => {
+          next(err);
+        });
+      });
 });
 
 module.exports = router;
